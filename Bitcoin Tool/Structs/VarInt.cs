7167ff901d954ac5d49dd12c2536ad2017edb944
doosterkamp <doosterkamp@dinamiqs.com>
2014-06-16 13:54:03 +0200

    Multiple changes: - split code into regions - added class, function and property discriptions - renamed some properties to make it more clear what they're used for - fixed some minor code formatting consistancy

--- Bitcoin Tool/Structs/VarInt.cs
+++ Bitcoin Tool/Structs/VarInt.cs
@@ -3,99 +3,184 @@ using System.IO;
 
 namespace BitCoin.Structs
 {
-	public class VarInt : ISerialize
-	{
-		public UInt64 value;
-		public Int32 intValue { get { return (Int32)value; } }
 
-		public VarInt() 
-		{
-		}
+    /// <summary>
+    /// Variable length integer. A integer that can be encoded depending on the represented value to save space.
+    /// </summary>
+    /// <remarks>
+    /// See https://en.bitcoin.it/wiki/Protocol_specification#Variable_length_integer for more detailed Information.
+    /// </remarks>
+    public class VarInt : ISerialize
+    {
 
-		public VarInt(Int64 i)
-		{
-			value = (UInt64)i;
-		}
+        /* ********************************************************************************
+        * Property declarations
+        * ****************************************************************************** */
+        #region "Property declarations"
 
-		public VarInt(UInt64 i)
-		{
-			value = i;
-		}
+        /// <summary>
+        /// VarInt value.
+        /// </summary>
+        public UInt64 value;
 
-		public VarInt(Byte[] b)
-		{
-			using (MemoryStream ms = new MemoryStream(b))
-			{
-				Read(ms);
-			}
-		}
+        /// <summary>
+        /// Standard 32bit Integer representation of the VarInt value.
+        /// </summary>
+        public Int32 intValue { get { return (Int32)value; } }
 
-		public void Read(Stream s)
-		{
-			BinaryReader br = new BinaryReader(s);
-				byte b = br.ReadByte();
-				if (b < 0xFD)
-					value = b;
-				else if (b == 0xFD)
-					value = br.ReadUInt16();
-				else if (b == 0xFE)
-					value = br.ReadUInt32();
-				else
-					value = br.ReadUInt64();
-		}
+        #endregion
 
-		public void Write(Stream s)
-		{
-			BinaryWriter bw = new BinaryWriter(s);
-				if (value < 0xFD)
-				{
-					bw.Write((Byte)value);
-				}
-				else if (value < UInt16.MaxValue)
-				{
-					bw.Write((Byte)0xFD);
-					bw.Write((UInt16)value);
-				}
-				else if (value < UInt32.MaxValue)
-				{
-					bw.Write((Byte)0xFE);
-					bw.Write((UInt32)value);
-				}
-				else
-				{
-					bw.Write((Byte)0xFF);
-					bw.Write((UInt64)value);
-			}
-		}
+        /* ********************************************************************************
+         * Class constructors
+         * ****************************************************************************** */
+        #region "Class constructors"
 
-		public Byte[] ToBytes() {
-			using (MemoryStream ms = new MemoryStream())
-			{
-				Write(ms);
-				return ms.ToArray();
-			}
-		}
+        /// <summary>
+        /// Clean initialization.
+        /// </summary>
+        public VarInt()
+        {
+        }
 
-		public static VarInt FromStream(Stream s)
-		{
-			VarInt x = new VarInt(0);
-			x.Read(s);
-			return x;
-		}
+        /// <summary>
+        /// Initializing with a Signed 64bit Integer
+        /// </summary>
+        /// <param name="i">Signed 64bit Integer value</param>
+        public VarInt(Int64 i)
+        {
+            this.value = (UInt64)i;
+        }
 
-		public static implicit operator VarInt(UInt64 i)
-		{
-			return new VarInt(i);
-		}
+        /// <summary>
+        /// Initializing with an Unsigned 64bit Integer
+        /// </summary>
+        /// <param name="i">Unsigned 64bit Integer value</param>
+        public VarInt(UInt64 i)
+        {
+            this.value = i;
+        }
 
-		public static implicit operator UInt64(VarInt vi)
-		{
-			return vi.value;
-		}
+        /// <summary>
+        /// Initialize with a preloaded byte array.
+        /// </summary>
+        /// <param name="byteArray">Byte array containing the contents.</param>
+        public VarInt(Byte[] byteArray)
+        {
+            using (MemoryStream _memorystream = new MemoryStream(byteArray))
+            {
+                this.Read(_memorystream);
+            }
+        }
 
-		public static implicit operator Int64(VarInt vi)
-		{
-			return (Int64)vi.value;
-		}
-	}
+        #endregion
+
+        /* ********************************************************************************
+         * Functions
+         * ****************************************************************************** */
+        #region "Functions"
+
+        /// <summary>
+        /// Load the contents of a stream into this structure.
+        /// </summary>
+        /// <param name="streamReference">Reference to the (predefined) stream.</param>
+        public void Read(Stream streamReference)
+        {
+            BinaryReader _binaryReader = new BinaryReader(streamReference);
+            byte _type = _binaryReader.ReadByte();
+            if (_type < 0xFD)
+                this.value = _type;
+            else if (_type == 0xFD)
+                this.value = _binaryReader.ReadUInt16();
+            else if (_type == 0xFE)
+                this.value = _binaryReader.ReadUInt32();
+            else
+                this.value = _binaryReader.ReadUInt64();
+        }
+
+        /// <summary>
+        /// (Re)write the contents of this structure into a stream.
+        /// </summary>
+        /// <param name="streamReference">Reference to the (predefined) stream.</param>
+        public void Write(Stream streamReference)
+        {
+            BinaryWriter _binarywriter = new BinaryWriter(streamReference);
+            if (this.value < 0xFD)
+            {
+                _binarywriter.Write((Byte)this.value);
+            }
+            else if (this.value < UInt16.MaxValue)
+            {
+                _binarywriter.Write((Byte)0xFD);
+                _binarywriter.Write((UInt16)this.value);
+            }
+            else if (this.value < UInt32.MaxValue)
+            {
+                _binarywriter.Write((Byte)0xFE);
+                _binarywriter.Write((UInt32)this.value);
+            }
+            else
+            {
+                _binarywriter.Write((Byte)0xFF);
+                _binarywriter.Write((UInt64)this.value);
+            }
+        }
+
+        /// <summary>
+        /// Export the contents of this structure into a byte array.
+        /// </summary>
+        /// <returns>Byte array containing the raw transaction data.</returns>
+        public Byte[] ToBytes()
+        {
+            using (MemoryStream _memorystream = new MemoryStream())
+            {
+                this.Write(_memorystream);
+                return _memorystream.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Read the contents of a (predefined) stream into a new copy of this structure.
+        /// </summary>
+        /// <param name="streamReference">Reference to the (predefined) stream.</param>
+        /// <returns>A new copy of this structure containing the transaction data.</returns>
+        public static VarInt FromStream(Stream streamReference)
+        {
+            VarInt _varint = new VarInt(0);
+            _varint.Read(streamReference);
+            return _varint;
+        }
+
+        /// <summary>
+        /// Extend the built-in operators or to provide conversion from a Unsigned 64bit Integer into the VarInt type.
+        /// </summary>
+        /// <param name="i">A Unsigned 64bit Integer value</param>
+        /// <returns>Value of the type VarInt</returns>
+        public static implicit operator VarInt(UInt64 i)
+        {
+            return new VarInt(i);
+        }
+
+        /// <summary>
+        /// Extend the built-in operators or to provide conversion from a VarInt into the Unsigned 64bit Integer type.
+        /// </summary>
+        /// <param name="i">Value of the type VarInt</param>
+        /// <returns>A Unsigned 64bit Integer value</returns>
+        public static implicit operator UInt64(VarInt vi)
+        {
+            return vi.value;
+        }
+
+        /// <summary>
+        /// Extend the built-in operators or to provide conversion from a VarInt into the Signed 64bit Integer type.
+        /// </summary>
+        /// <param name="i">Value of the type VarInt</param>
+        /// <returns>A Signed 64bit Integer value</returns>
+        public static implicit operator Int64(VarInt vi)
+        {
+            return (Int64)vi.value;
+        }
+
+        #endregion
+
+    }
 }
